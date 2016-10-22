6c4fbf6e8109708fb0b78f67c5af644a6a837515
Aman Rana <amanrana20@gmail.com>
2016-10-22 17:05:22 -0400

    make small changes in colvolution layer

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -34,16 +34,16 @@ OPTIMIZER = 'adam'
 def baseline_model():
 	# create model
 	model = Sequential()
-	model.add(Convolution2D(32, 5, 5, border_mode='valid', input_shape=(1, 28, 28), activation='relu'))
+	model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(1, 28, 28), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
-    	model.add(Convolution2D(64, 5, 5, border_mode='valid', activation='relu'))
-    	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.2))
 	model.add(Flatten())
-        model.add(Dense(128, activation='relu'))
+	model.add(Dense(128, activation='relu'))
 	model.add(Dense(num_classes, activation='softmax'))
 	# Compile model
-        model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=['accuracy'])
+	model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=['accuracy'])
 	return model
 
 EPOCH = 2
@@ -55,5 +55,5 @@ print model.summary()
 # Fit the model
 history = model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=EPOCH, batch_size=BATCH_SIZE, verbose=2)
 # Final evaluation of the model
-#scores = model.evaluate(X_test, y_test, verbose=0)
+scores = model.evaluate(X_test, y_test, verbose=0)
 print("CNN Error: %.2f%%" % (100-scores[1]*100))
